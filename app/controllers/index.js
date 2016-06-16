b2a96b87321fc0e973570f78d05a741177715029
Somprasong Intorruk <pre.mos_os@hotmail.com>
2016-06-16 14:13:21 +0700

    M05 TEST CODE

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,23 @@
-function doClick(e) {
-    alert($.label.text);
+$.index.open();
+
+function btnClick(e) {
+   var toast = Ti.UI.createNotification({
+        message: "Toast lol",
+        duration: Ti.UI.NOTIFICATION_DURATION_LONG
+    });
+    
+    toast.show(); 
 }
 
-$.index.open();
+function imgClick(e) {	
+	$.image.animate({
+		opacity:0,
+		duration:300
+	}, function(){
+		$.image.animate({
+			opacity:1,
+			duration:500
+		});	
+	});	
+}
+
