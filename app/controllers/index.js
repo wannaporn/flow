4fbe9f2080cfc179d74995874e5d498d22ec1910
paramade <paramade@gmail.com>
2016-06-16 13:38:42 +0700

    Test_mycode

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,16 @@
 function doClick(e) {
-    alert($.label.text);
+	Titanium.API.info("You clicked the button");
+    alert("Hi Paramade");
 }
+function doClick2(e) {
+	Titanium.API.info("You clicked the picture");
+    alert("I love chloe");
+}
+
+$.slider.text = $.slider.value;
+function updateLabel(e){
+    $.label.text = String.format("%3.1f", e.value);
+}
+$.win.open();
 
 $.index.open();
