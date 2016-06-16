77afb68214387b623a1701bb17c60e86d3128a8c
pondking <paranyoo-o@hotmail.com>
2016-06-16 11:40:57 +0700

    test code
    TabGroup and Listview

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,9 @@
 function doClick(e) {
     alert($.label.text);
 }
-
+function animateOpen() {
+    Alloy.createController('win2').getView().open({
+        transition: Ti.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT
+    });
+}
 $.index.open();
