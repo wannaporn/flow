f869e98c038b5878e0b355b9f6c53ef3403e63dd
cannoncn <cannoncn@gmail.com>
2016-06-16 18:42:21 +0700

    NON_work1

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,30 @@
-function doClick(e) {
-    alert($.label.text);
+var i = 0;
+var pathImg = ["/images/11.jpg","/images/12.jpg","/images/13.jpg"];
+function imgClick(e) {  
+   
+    if ( i >= pathImg.length - 1 ){
+        i = -1;
+    }
+    
+    $.slideme.animate({
+        opacity:0,
+        duration:300
+    }, 
+    function(){
+        $.slideme.image = pathImg[i];
+        $.slideme.animate({
+            opacity:1,
+            duration:400
+        }); 
+    }); 
+    
+    setTimeout(imgClick,1000);
+    i++;
 }
 
+
+setTimeout(imgClick,2000);
+
 $.index.open();
+
+
