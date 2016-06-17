024701f7d838d85e2a967a754ef3723a7a27c85c
pondking <paranyoo-o@hotmail.com>
2016-06-17 11:12:24 +0700

    update code layout
    จัดโค้ดใหม่ครับ

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,7 @@
 function doClick(e) {
     alert($.label.text);
 }
+
 function animateOpen() {
     Alloy.createController('win2').getView().open({
         transition: Ti.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT
